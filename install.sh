34193d2d5619f04e64985c2f429d87233f57d6c4
kuya <kuya@gwu.edu>
2017-05-28 13:27:43 -0400

    anaconda install change

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-apt-get update
-apt-get -y install git\
+sudo apt-get update
+sudo apt-get -y install git\
 	tmux\
 	cmake\
 	curl\
@@ -16,40 +16,48 @@ apt-get -y install git\
     xclip\
     build-essential
 
-if [ ! -d "~/anaconda" ]; then
+if [ ! -d "~/anaconda3" ]; then
 	wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh -O ~/anaconda.sh
 	bash ~/anaconda.sh -b -p $HOME/anaconda
-	export PATH="$HOME/anaconda/bin:$PATH"
+	export PATH="$HOME/anaconda3/bin:$PATH"
 fi
 
 
-export PATH="$HOME/anaconda/bin:$PATH"
+export PATH="$HOME/anaconda3/bin:$PATH"
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
 
 pip install -U pip
 pip install powerline-status
 pip install powerline-gitstatus 
 
-#Get or update neovim github repo
-if [ ! -d "~/src" ]; then
-	mkdir -p ~/src
-fi
-cd ~/src
-if [ ! -e ~/src/neovim ]; then
-  git clone https://github.com/neovim/neovim.git
-else
-  cd neovim
-  git pull origin
-fi
+# neovim install
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
 
-cd ~/src/neovim
+#Get or update neovim github repo
+#if [ ! -d "~/src" ]; then
+#	mkdir -p ~/src
+#fi
+#cd ~/src
+#if [ ! -e ~/src/neovim ]; then
+#  git clone https://github.com/neovim/neovim.git
+#else
+#  cd neovim
+#  git pull origin
+#fi
+
+#cd ~/src/neovim
 #Checkout stable release
 # git checkout v0.1.7
 #Remove old build dir
 #rm -r build/
 
 # Build and install neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
+#make CMAKE_BUILD_TYPE=RelWithDebInfo
+#make install
 
 # Enable use of python plugins
 # pip2 install --user --upgrade neovim
@@ -60,8 +68,8 @@ apt-get install pandoc -y
 ##apt-get update
 ##apt-get install neovim
 ## bash Anaconda3
-os="$(uname)"
-echo "$os"
+#os="$(uname)"
+#echo "$os"
 ## if["$os"=="Linux"]; then
 ##  echo "Install neovim from os repositories"
 ## fi
