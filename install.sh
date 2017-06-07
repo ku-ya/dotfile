64640a7b87dda1a6eb36d3032001958c2e8053ff
kuya <kuya@gwu.edu>
2017-06-06 23:26:45 -0400

    fix gitconfig limiting to repo ssh

--- install.sh
+++ install.sh
@@ -29,38 +29,37 @@ sudo apt-get install python-dev python-pip python3-dev python3-pip
 
 pip install -U pip
 pip install powerline-status
-pip install powerline-gitstatus 
+pip install powerline-gitstatus
 
 # neovim install
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:neovim-ppa/stable
+#sudo apt-get update
+#sudo apt-get install neovim
 
 #Get or update neovim github repo
-#if [ ! -d "~/src" ]; then
-#	mkdir -p ~/src
-#fi
-#cd ~/src
-#if [ ! -e ~/src/neovim ]; then
-#  git clone https://github.com/neovim/neovim.git
-#else
-#  cd neovim
-#  git pull origin
-#fi
+if [ ! -d "~/src" ]; then
+	mkdir -p ~/Documents/src
+fi
+cd ~/Documents/src
+if [ ! -e ~/Documents/src/neovim ]; then
+  git clone https://github.com/neovim/neovim.git
+else
+  cd neovim
+fi
 
-#cd ~/src/neovim
+cd ~/Documents/src/neovim
 #Checkout stable release
 # git checkout v0.1.7
 #Remove old build dir
 #rm -r build/
 
 # Build and install neovim
-#make CMAKE_BUILD_TYPE=RelWithDebInfo
-#make install
-
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+pip3 install neovim
 # Enable use of python plugins
-# pip2 install --user --upgrade neovim
+#pip2 install --user --upgrade neovim
 apt-get install pandoc -y
 
 ##	neovim\
